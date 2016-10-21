02f77d6ef6b7336800a38b9ed9ef44a4bb5ed0f1
Per Christian Henden <perhenden@gmail.com>
2016-10-21 19:56:42 +0200

    Add app module

---
+++ app/src/main/java/org/digitalsprouts/recipesearch/RecipeSearchActivityFragment.java
@@ -0,0 +1,22 @@
+package org.digitalsprouts.recipesearch;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class RecipeSearchActivityFragment extends Fragment {
+
+    public RecipeSearchActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_recipe_search, container, false);
+    }
+}
